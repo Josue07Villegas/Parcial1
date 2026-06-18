081001c8c0e02ca2e91ef30ef48ca92612667dc3
Josue07Villegas <pepitovillegas07@hotmail.com>
2026-06-17 19:53:00 -0600

    Agregando el Formato incial

---
+++ calculadora.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void suma();
+float resta(float,float);
+void multiplicacion();
+float division(float,float);
+int main(){
+	float a,b;
+	printf("calculadora sencilla");
+	return 0;
+}
+
